90c83688fd9ad0ded4fdaccfabf38f0eb79abf6a
HakimHisham1991 <hakim.hisham.1991@gmail.com>
2025-07-20 11:48:37 +0800

    Update gcodeConfig.js

--- js/gcodeConfig.js
+++ js/gcodeConfig.js
@@ -1,4 +1,9 @@
+console.log('✅ loaded: ' + new URL(import.meta.url).pathname.split('/').pop());
+
+/**
+ * Configuration for valid G-code commands
+ * @module gcodeConfig
+ */
 export const validCodes = new Set([
   'G00', 'G0', 'G01', 'G1', 'G02', 'G2', 'G03', 'G3'
-  // Add future codes here, e.g., 'G04', 'G17', 'G90', 'M03', 'M30'
 ]);

4c796b01c1235c0fa8d1c8698dbfcd7a03b2820f
HakimHisham1991 <hakim.hisham.1991@gmail.com>
2025-08-01 19:53:55 +0800

    Delete js directory

--- js/gcodeConfig.js
+++
@@ -1,9 +0,0 @@
-console.log('✅ loaded: ' + new URL(import.meta.url).pathname.split('/').pop());
-
-/**
- * Configuration for valid G-code commands
- * @module gcodeConfig
- */
-export const validCodes = new Set([
-  'G00', 'G0', 'G01', 'G1', 'G02', 'G2', 'G03', 'G3'
-]);
